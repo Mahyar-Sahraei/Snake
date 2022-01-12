e93afae61541db40d153b5783ccddc87a825ef86
Mahdifm1 <80mahde@gmail.com>
2022-01-12 19:38:59 +0330

    add menu text size & fix bugs

--- SourceFiles/Menu.cpp
+++ SourceFiles/Menu.cpp
@@ -13,13 +13,15 @@ Menu::Menu(float width, float length) {
 
     text[0].setColor(sf::Color::Red);
     text[0].setFont(font);
-    text[0].setPosition(sf::Vector2f(width / 2, length / (NUMBER_OF_ITEMS + 1) * 1));
+    text[0].setPosition(sf::Vector2f(width / 2 -70, length / (NUMBER_OF_ITEMS +2) * 1));
     text[0].setString("Start");
+    text[0].setCharacterSize(60);
 
     text[1].setColor(sf::Color::White);
     text[1].setFont(font);
-    text[1].setPosition(sf::Vector2f(width / 2, length / (NUMBER_OF_ITEMS + 1) * 2));
+    text[1].setPosition(sf::Vector2f(width / 2 -50, length / (NUMBER_OF_ITEMS +2) * 2));
     text[1].setString("Exit");
+    text[1].setCharacterSize(60);
 }
 
 void Menu::moveUpWithKeyboard() {
@@ -27,13 +29,21 @@ void Menu::moveUpWithKeyboard() {
         text[selectedItem].setColor(sf::Color::White);
         selectedItem--;
         text[selectedItem].setColor(sf::Color::Red);
+    } else {
+        text[selectedItem].setColor(sf::Color::White);
+        selectedItem++;
+        text[selectedItem].setColor(sf::Color::Red);
     }
 }
 
 void Menu::moveDownWhiteKeyboard() {
-    if (selectedItem < NUMBER_OF_ITEMS) {
+    if (selectedItem < NUMBER_OF_ITEMS-1) {
         text[selectedItem].setColor(sf::Color::White);
         selectedItem++;
+        text[selectedItem].setColor(sf::Color::Red);
+    } else {
+        text[selectedItem].setColor(sf::Color::White);
+        selectedItem--;
         text[selectedItem].setColor(sf::Color::Red);
     }
 }
