faf02bac4bed45b05a9713769d72a3fe945d2cff
Mahdifm1 <80mahde@gmail.com>
2022-01-13 16:32:29 +0330

    change mainWin loop

--- SourceFiles/MainWin.cpp
+++ SourceFiles/MainWin.cpp
@@ -1,62 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include "HeaderFiles/board.hpp"
+#include "HeaderFiles/initializer.hpp"
 #include "HeaderFiles/Tiles.hpp"
 #include "HeaderFiles/snake.hpp"
-#include "HeaderFiles/stone.hpp"
-#include "HeaderFiles/fruit.hpp"
-#include "Menu.hpp"
+#include "HeaderFiles/Menu.hpp"
 
 int main() {
     sf::RenderWindow mainWin(sf::VideoMode(B_SIZE * T_SIZE, B_SIZE * T_SIZE), "Snake!");
     mainWin.setFramerateLimit(1);
-    Menu menu(mainWin.getSize().x, mainWin.getSize().y);
-    Snake snake1, snake2;
-    Stone stone;
-    Fruit fruit;
-    Board board({snake1, snake2, stone, fruit});
+    Menu menu(mainWin.getSize().x,mainWin.getSize().y);
+    Board board;
+    Initializer initializer;
+    std::pair<sf::Vector2f, sf::Vector2f> snakesPos =
+            initializer.initiate(board.getStateMap());
+    Snake snake1(Obj::Snake1, snakesPos.first);
+    Snake snake2(Obj::Snake2, snakesPos.second);
     Tiles map;
-    map.load(board.stateMap);
+    map.load(board.getStateMap());
     while (mainWin.isOpen()) {
         sf::Event event;
         while (mainWin.pollEvent(event)) {
-            sf::Event event;
-            while (mainWin.pollEvent(event)) {
-                switch (event.type) {
-                    case sf::Event::Closed:
-                        mainWin.close();
-                        break;
-
-                    case sf::Event::KeyReleased:
-                        switch (event.key.code) {
-                            case sf::Keyboard::Escape:
-                                mainWin.close();
-                                break;
-
-                            case sf::Keyboard::Up:
-                                menu.moveUpWithKeyboard();
-                                break;
-
-                            case sf::Keyboard::Down:
-                                menu.moveDownWhiteKeyboard();
-                                break;
-
-                            case sf::Keyboard::Return:
-                                switch (menu.getPressedItem()) {
-                                    case 0:
-                                        //start game
-                                        break;
-                                    case 1:
-                                        mainWin.close();
-                                        break;
-                                }
-                                break;
-                        }
-                        break;
-                }
+            if (event.type == sf::Event::Closed) {
+                mainWin.close();
             }
-            map.load(board.stateMap);
-            mainWin.clear(sf::Color::Black);
-            mainWin.draw(map);
-            mainWin.display();
         }
+        map.load(board.getStateMap());
+        mainWin.clear(sf::Color::Black);
+        mainWin.draw(map);
+        mainWin.display();
     }
 }
