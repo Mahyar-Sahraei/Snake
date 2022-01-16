cb1cd23e27d6aa77673fcd48809392eccab3f694
Mahyar-Sahraei <sahraei19@gmail.com>
2022-01-16 12:22:29 +0330

    changed setColor() to setFillColor()

--- SourceFiles/gameOverMenu.cpp
+++ SourceFiles/gameOverMenu.cpp
@@ -11,13 +11,13 @@ gameOverMenu::gameOverMenu(float width, float length) {
         //error
     }
 
-    text[0].setColor(sf::Color::Red);
+    text[0].setFillColor(sf::Color::Red);
     text[0].setFont(font);
     text[0].setPosition(sf::Vector2f(width / 2 -70, length / (NUMBER_OF_ITEMS +2) * 1));
     text[0].setString("Start");
     text[0].setCharacterSize(70);
 
-    text[1].setColor(sf::Color::Yellow);
+    text[1].setFillColor(sf::Color::Yellow);
     text[1].setFont(font);
     text[1].setPosition(sf::Vector2f(width / 2 -50, length / (NUMBER_OF_ITEMS +2) * 2));
     text[1].setString("Exit");
@@ -26,25 +26,25 @@ gameOverMenu::gameOverMenu(float width, float length) {
 
 void gameOverMenu::moveUpWithKeyboard() {
     if (selectedItem > 0) {
-        text[selectedItem].setColor(sf::Color::Yellow);
+        text[selectedItem].setFillColor(sf::Color::Yellow);
         selectedItem--;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     } else {
-        text[selectedItem].setColor(sf::Color::Yellow);
+        text[selectedItem].setFillColor(sf::Color::Yellow);
         selectedItem++;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     }
 }
 
 void gameOverMenu::moveDownWhiteKeyboard() {
     if (selectedItem < NUMBER_OF_ITEMS-1) {
-        text[selectedItem].setColor(sf::Color::Yellow);
+        text[selectedItem].setFillColor(sf::Color::Yellow);
         selectedItem++;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     } else {
-        text[selectedItem].setColor(sf::Color::Yellow);
+        text[selectedItem].setFillColor(sf::Color::Yellow);
         selectedItem--;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     }
 }
 
