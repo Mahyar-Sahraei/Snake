7ac7bdca062f18ef9e45ec54ce8edb1b17563486
Mahyar-Sahraei <sahraei19@gmail.com>
2022-01-19 01:49:19 +0330

    changed setColor to setFillColor.

--- SourceFiles/Menu.cpp
+++ SourceFiles/Menu.cpp
@@ -11,19 +11,19 @@ Menu::Menu(float width, float length) {
         //error
     }
 
-    text[0].setColor(sf::Color::Red);
+    text[0].setFillColor(sf::Color::Red);
     text[0].setFont(font);
     text[0].setPosition(sf::Vector2f(width / 2 -70, length / (NUMBER_OF_ITEMS +2) * 1.7));
     text[0].setString("Start");
     text[0].setCharacterSize(70);
 
-    text[1].setColor(sf::Color::White);
+    text[1].setFillColor(sf::Color::White);
     text[1].setFont(font);
     text[1].setPosition(sf::Vector2f(width / 2 -50, length / (NUMBER_OF_ITEMS +2) * 2.4));
     text[1].setString("Exit");
     text[1].setCharacterSize(70);
 
-    gameName.setColor(sf::Color::Black);
+    gameName.setFillColor(sf::Color::Black);
     gameName.setFont(font);
     gameName.setPosition(sf::Vector2f(width / 2 -190, length / (NUMBER_OF_ITEMS +2) - 100));
     gameName.setString("Snake Game");
@@ -32,25 +32,25 @@ Menu::Menu(float width, float length) {
 
 void Menu::moveUpWithKeyboard() {
     if (selectedItem > 0) {
-        text[selectedItem].setColor(sf::Color::White);
+        text[selectedItem].setFillColor(sf::Color::White);
         selectedItem--;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     } else {
-        text[selectedItem].setColor(sf::Color::White);
+        text[selectedItem].setFillColor(sf::Color::White);
         selectedItem++;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     }
 }
 
 void Menu::moveDownWhiteKeyboard() {
     if (selectedItem < NUMBER_OF_ITEMS-1) {
-        text[selectedItem].setColor(sf::Color::White);
+        text[selectedItem].setFillColor(sf::Color::White);
         selectedItem++;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     } else {
-        text[selectedItem].setColor(sf::Color::White);
+        text[selectedItem].setFillColor(sf::Color::White);
         selectedItem--;
-        text[selectedItem].setColor(sf::Color::Red);
+        text[selectedItem].setFillColor(sf::Color::Red);
     }
 }
 
