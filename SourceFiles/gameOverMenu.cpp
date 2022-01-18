aa4a16a1a6624cf6933d357b34da09d209eb168d
Mahdifm1 <80mahde@gmail.com>
2022-01-18 18:08:43 +0330

    Update gameOverMenu texts positions

--- SourceFiles/gameOverMenu.cpp
+++ SourceFiles/gameOverMenu.cpp
@@ -1,7 +1,7 @@
 #include "HeaderFiles/gameOverMenu.hpp"
 
 gameOverMenu::gameOverMenu(float width, float length) {
-    backGround.setSize(sf::Vector2f (width,length));
+    backGround.setSize(sf::Vector2f(width, length));
     if (!backGroundTexture.loadFromFile("gameOverBackGround.png")) {
         //error
     }
@@ -13,15 +13,15 @@ gameOverMenu::gameOverMenu(float width, float length) {
 
     text[0].setFillColor(sf::Color::Red);
     text[0].setFont(font);
-    text[0].setPosition(sf::Vector2f(width / 2 -70, length / (NUMBER_OF_ITEMS +2) * 1));
+    text[0].setPosition(sf::Vector2f(width / 2 - 80, length / (NUMBER_OF_ITEMS + 2) * 1 - 10));
     text[0].setString("Start");
     text[0].setCharacterSize(70);
 
     text[1].setFillColor(sf::Color::Yellow);
     text[1].setFont(font);
-    text[1].setPosition(sf::Vector2f(width / 2 -50, length / (NUMBER_OF_ITEMS +2) * 2));
+    text[1].setPosition(sf::Vector2f(width / 2 - 55, length / (NUMBER_OF_ITEMS + 2) * 2 + 80));
     text[1].setString("Exit");
-    text[1].setCharacterSize(50);
+    text[1].setCharacterSize(70);
 }
 
 void gameOverMenu::moveUpWithKeyboard() {
@@ -37,7 +37,7 @@ void gameOverMenu::moveUpWithKeyboard() {
 }
 
 void gameOverMenu::moveDownWhiteKeyboard() {
-    if (selectedItem < NUMBER_OF_ITEMS-1) {
+    if (selectedItem < NUMBER_OF_ITEMS - 1) {
         text[selectedItem].setFillColor(sf::Color::Yellow);
         selectedItem++;
         text[selectedItem].setFillColor(sf::Color::Red);
